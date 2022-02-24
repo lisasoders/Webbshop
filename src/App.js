506b9fa290929c144b5882e4a433116757b48431
lisa <lisasoders@gmail.com>
2022-02-24 10:10:48 +0100

    uppstart

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import Products from './pages/Products';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Webshop in React
+      <Products />
+      
+    </div>
+  );
+}
+
+export default App;
