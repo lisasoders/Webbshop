2e6e1136f2458c74fa1f40eceb5907fc70eea8c9
lisa <lisasoders@gmail.com>
2022-02-25 15:33:39 +0100

    Lagt till routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-
-
-
+import { Link } from 'react-router-dom'
 
 function Products() {
 
@@ -26,22 +24,25 @@ const handleClick = (product) => {
   console.log(product)
   
 }
+
   return (
-  
+ 
     <div>
       <h1>Products</h1>
       {products.map((product)=> (
         <div>
-          <img src={product.url} alt="cars"></img>
-          <p>{product.title}</p>
+         <img className="product-img" src={product.url} alt="cars"></img>
+          <p><Link to={`/product/${product.title}`}>{product.title}</Link></p>
           <p>{product.price} Kr</p>
           <button onClick={() => handleClick(product)}>Add to cart</button>
         </div>
       ))}
     </div>
    
+   
   )
 }
+
 
 
 export default Products
